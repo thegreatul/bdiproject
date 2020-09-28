eaf4c862ad4d6605719ad697a2e25669fb1756b5
thegreatul <agusjuliardi@gmail.com>
2020-09-28 16:27:50 +0800

    Create dataAsuransiMobil.ts

---
+++ bancassurance/src/app/dataAsuransiMobil.ts
@@ -0,0 +1,38 @@
+export const dataAsuransi = [
+  {
+    "asuransi_1": 
+      {
+        "nama_produk": "Total Loss Only",
+        "merk":"Autocilin",
+        "konten_1":{
+            "judul":"Perlindungan Kerusakan Total",
+            "deskripsi":"Uang pertanggungan sampai dengan IDR 126.000.000"
+            },
+        "konten_2":{
+            "judul":"Fasilitas Derek Autocilin",
+            "deskripsi":"Layanan Mobil derek gratis dari Autocilin untuk kecelakaan kendaraan total IDR 554.400"
+            }
+       },
+    "asuransi_2":
+      {
+        "nama_produk": "Comprehensive (All Risk)",
+        "merk":"Autocilin",
+        "konten_1":{
+            "judul":"Perlindungan segala Jenis Kerusakan",
+            "deskripsi":"Uang pertanggungan sampai dengan IDR 126.000.000"
+            },
+        "konten_2":{
+            "judul":"Bengkel Rekanan Terpercaya",
+            "deskripsi":"Jaminan keaslian suku cadang dan garansi 6 bulan setelah perbaikan"
+            },
+        "konten_3":{
+            "judul":"Emergency Roadside Assistance & Fasilitas Derek",
+            "deskripsi":"Layanan gratis untuk gangguan darurat, termasuk layanan derek untuk kecelakaan & kerusaan mesin"
+            },
+        "konten_4":{
+            "judul":"Same Day Repair",
+            "deskripsi":"Perbaikan di hari yang sama untuk kerusakan ringan pada bengkel rekanan terpilih"
+            }
+       }
+  }
+];
